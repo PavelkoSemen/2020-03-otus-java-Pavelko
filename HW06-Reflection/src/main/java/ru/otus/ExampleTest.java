2925f62346f5d7ee339731af94b3f878ce7da9db
Semen Pavelko <se.pa.94@mail.ru>
2020-04-25 02:01:31 +0300

    Error correction HW06

--- HW06-Reflection/src/main/java/ru/otus/ExampleTest.java
+++ HW06-Reflection/src/main/java/ru/otus/ExampleTest.java
@@ -11,8 +11,24 @@ public class ExampleTest {
     }
 
     @Before
+    public void Before1() {
+        System.out.println("This is Before1");
+    }
+
+    @Before
+    public void Before2() {
+        System.out.println("This is Before2");
+    }
+
+    @Before
+    public void Before3() {
+        System.out.println("This is Before3");
+    }
+
+    @Test
     public void method1() {
         System.out.println("This is method1");
+
     }
 
     @Test
@@ -24,37 +40,48 @@ public class ExampleTest {
     @Test
     public void method3() {
         System.out.println("This is method3");
-
-    }
-    @Test
-    public void method4() {
-        System.out.println("This is method3");
         throw new NullPointerException();
     }
+
     @Test
-    public void method5() {
-        System.out.println("This is method3");
+    public void method4() {
+        System.out.println("This is method4");
 
     }
+
     @Test
-    public void method6() throws ParsingException {
-        System.out.println("This is method3");
+    public void method5() throws ParsingException {
+        System.out.println("This is method5");
         throw new ParsingException();
     }
+
     @Test
-    public void method7() {
-        System.out.println("This is method3");
+    public void method6() {
+        System.out.println("This is method6");
 
     }
+
     @Test
-    public void method8() {
-        System.out.println("This is method3");
+    public void method7() {
+        System.out.println("This is method7");
 
     }
 
     @After
-    public void method9() {
-        System.out.println("This is method4");
+    public void After1() {
+        System.out.println("This is After1");
+
+    }
+
+    @After
+    public void After2() {
+        System.out.println("This is After2");
+
+    }
+
+    @After
+    public void After3() {
+        System.out.println("This is After3");
 
     }
 }
--- HW06-Reflection/src/main/java/ru/otus/Main.java
+++ HW06-Reflection/src/main/java/ru/otus/Main.java
@@ -3,7 +3,7 @@ package ru.otus;
 public class Main {
     public static void main(String[] args) {
 
-        new RunTests(ExampleTest.class).runningTests();
+        new TestRunner(ExampleTest.class).runningTests();
 
     }
 }
