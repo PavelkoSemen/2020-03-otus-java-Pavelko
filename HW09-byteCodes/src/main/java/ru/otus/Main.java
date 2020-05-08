4172400b7faa3de32b1adb618ce794b1596f9ccb
Semen Pavelko <se.pa.94@mail.ru>
2020-05-08 07:51:59 +0300

    Error correction HW09

--- HW09-byteCodes/src/main/java/ru/otus/Main.java
+++ HW09-byteCodes/src/main/java/ru/otus/Main.java
@@ -3,10 +3,14 @@ package ru.otus;
 
 import ru.otus.logger.TestClass;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        new TestClass().testMethod1(26);
-        new TestClass().testMethod2(12);
-        new TestClass().testMethod3(1991);
+        new TestClass().testMethod1(26,"sd",23);
+        new TestClass().testMethod2("sd",26);
+        new TestClass().testMethod3(26,23);
+        new TestClass().testMethod4();
+
     }
 }
--- HW09-byteCodes/src/main/java/ru/otus/logger/Agent.java
+++ HW09-byteCodes/src/main/java/ru/otus/logger/Agent.java
@@ -2,19 +2,12 @@ package ru.otus.logger;
 
 import org.objectweb.asm.*;
 
-
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
-import java.lang.invoke.CallSite;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
 import java.security.ProtectionDomain;
-
-
-import static org.objectweb.asm.Opcodes.H_INVOKESTATIC;
 
 public class Agent {
     public static void premain(String agentArgs, Instrumentation inst) {
@@ -33,30 +26,12 @@ public class Agent {
         });
     }
 
-
     private static byte[] addProxyMethod(byte[] originalClass) {
         ClassReader cr = new ClassReader(originalClass);
         ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_MAXS);
         MyClassVisitor cv = new MyClassVisitor(cw);
 
         cr.accept(cv, Opcodes.ASM5);
-
-        MethodVisitor mv = cw.visitMethod(Opcodes.ACC_PUBLIC, "log", "(Ljava/lang/String;I)V", null, null);
-        Handle handle = new Handle(
-                H_INVOKESTATIC,
-                Type.getInternalName(java.lang.invoke.StringConcatFactory.class),
-                "makeConcatWithConstants",
-                MethodType.methodType(CallSite.class, MethodHandles.Lookup.class, String.class, MethodType.class, String.class, Object[].class).toMethodDescriptorString(),
-                false);
-        mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
-        mv.visitVarInsn(Opcodes.ALOAD, 1);
-        mv.visitVarInsn(Opcodes.ILOAD, 2);
-        mv.visitInvokeDynamicInsn("makeConcatWithConstants", "(Ljava/lang/String;I)Ljava/lang/String;", handle, "Executed method: \u0001, param: \u0001");
-        mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
-
-        mv.visitInsn(Opcodes.RETURN);
-        mv.visitMaxs(0, 0);
-        mv.visitEnd();
 
 
         byte[] finalClass = cw.toByteArray();
@@ -70,6 +45,5 @@ public class Agent {
 
         return finalClass;
     }
-
 
 }
