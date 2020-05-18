22c073d9983547ebfca6845aa2638e98ec057ebe
PavelkoSemen <se.pa.94@mail.ru>
2020-05-18 21:14:28 +0300

    Revert "HW09-byteCodes"

--- HW10-ATM/src/main/java/ru/otus/demoATM/BalanceReplenishment.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.demoATM;
-
-import java.util.Map;
-
-public interface BalanceReplenishment {
-    void replenishBalance(Map<Integer, Integer> balance, Integer cash);
-}
--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -1,5 +1,3 @@
 public interface Versions {
     String guava = "28.2-jre";
-    String asm = "8.0.1";
 }
-  
